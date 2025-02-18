20aa32ec77857b53b2c44172fa0f7c49f072b242
Elisa Mcgarry <elisa.mcgarry@aviva.com>
2025-02-18 10:22:06 +0000

    convert js to ts

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+// import disco_ball from './assets/disco_ball.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <body className='homepage'>
+      <div className='introduction'>
+        {/* <img src={disco_ball} className="logo react" alt="React logo" /> */}
+        <h1>Welcome to Disco DJ</h1>
+        </div>
+    </body>  
+    </>
+  )
+}
+
+export default App
